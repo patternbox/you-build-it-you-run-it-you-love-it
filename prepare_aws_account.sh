d7cb0a34a117f694e5f094fb123c184f1570287a
Patternbox <ehms@patternbox.com>
2023-06-10 18:22:02 +0200

    Cache cdk.out folder

--- prepare_aws_account.sh
+++ prepare_aws_account.sh
@@ -4,6 +4,6 @@ AWS_REGION=$(aws configure get region)
 AWS_ACCOUNT=$(aws sts get-caller-identity --query "Account" --output text)
 echo "AWS_Account: $AWS_ACCOUNT, AWS_Region: $AWS_REGION"
 
-cdk bootstrap aws://$AWS_ACCOUNT/$AWS_REGION
-
 aws-infra/01_iam/cfn_github_deployer.sh
+
+cdk bootstrap aws://$AWS_ACCOUNT/$AWS_REGION
